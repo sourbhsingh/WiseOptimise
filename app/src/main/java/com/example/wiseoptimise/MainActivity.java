1defd95d1595c02c249092c579c8a34d83669ac3
sourbhsingh <17sourav.singh@gmail.com>
2023-03-01 22:47:41 +0530

    bluetooth disabled msg

--- app/src/main/java/com/example/wiseoptimise/MainActivity.java
+++ app/src/main/java/com/example/wiseoptimise/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.wiseoptimise;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.BatteryManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar ;
+    private BluetoothAdapter bAdapter;
     private TextView showBatteryPercentage;
     private BroadcastReceiver fetchBatteryPercentage = new BroadcastReceiver() {
         @Override
@@ -29,5 +39,34 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar5);
         this.registerReceiver(this.fetchBatteryPercentage, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
+    }
+
+    public void optimiseBattery(View view) {
+   disableBluetooth();
+
+
+
+
+
+    }
+
+    public void disableBluetooth(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT)== PackageManager.PERMISSION_DENIED){
+            if(Build.VERSION.SDK_INT>=31){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.BLUETOOTH_CONNECT},100);
+                return ;
+            }
+        }
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        if(Build.VERSION.SDK_INT>=31){
+            bAdapter = bluetoothManager.getAdapter();
+        }
+        else bAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bAdapter.isEnabled()) {
+            bAdapter.disable();
+            Toast.makeText(this, "Bluetooth Disabled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
